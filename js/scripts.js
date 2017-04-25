3a0cdc2528f53358f79cd4392e45b95ddf33847c
marblo <marcinblochowiak@wp.pl>
2017-04-25 20:55:29 +0200

    first step

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+
+var a = 5
+var h = 6
+var triangleArea = a*h/2
+var name = prompt('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);

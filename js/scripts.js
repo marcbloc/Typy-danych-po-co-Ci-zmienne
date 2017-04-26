a1a0c911a493e1ee7959517fe395348cab738cbf
marblo <marcinblochowiak@wp.pl>
2017-04-26 17:18:51 +0200

    second step

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 
-var a = 5
-var h = 6
+var a = prompt('dodaj długość')
+	if 
+		(isNaN(a)) {alert('to nie liczba');}
+	else
+		{alert('to liczba');}
+var h = prompt('podaj wysokość')
+	if 
+		(isNaN(a)) {alert('to nie liczba');}
+	else
+		{alert('to liczba');}
 var triangleArea = a*h/2
-var name = prompt('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+	alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);

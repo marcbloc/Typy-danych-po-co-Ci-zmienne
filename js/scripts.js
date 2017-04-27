715e3209cdde84bc6ef7a83a54c32fd81d9934ad
marblo <marcinblochowiak@wp.pl>
2017-04-27 17:23:15 +0200

    test2

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
+var a;
+var b;
+var triangleArea;
 
-var a = prompt('dodaj długość');
-if 
-	(isNaN(a)) {alert('to nie liczba');
-}else
-	{alert('to liczba');}
-var b = prompt('podaj wysokość');
-if 
-	(isNaN(b)) {alert('to nie liczba');
-}else
-	{alert('to liczba');}
-var triangleArea = a*h/2
-	alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+a = prompt('dodaj długość');
+if (isNaN(a)) {
+	alert('to nie liczba');
+} else { 
+	alert('to liczba');
+}
+b = prompt('podaj wysokość');
+if (isNaN(b)) {
+	alert('to nie liczba');
+} else {
+	alert('to liczba');
+}
+triangleArea = (a*h/2);
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 
 
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
